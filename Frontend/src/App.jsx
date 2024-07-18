d537185243b3eb0bfe7399d993687c9fe7af9b54
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-07-18 08:32:20 +0530

    setup shadcn ui in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl bg-blue-400 font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
     </div>
   );
 }
