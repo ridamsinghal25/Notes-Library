d73e74a8fa339686327d003d1bcdd32cc79379f5
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-07-19 16:34:04 +0530

    add home page to codebase

---
+++ Frontend/src/constants/home.constants.js
@@ -0,0 +1,17 @@
+// Home constants
+
+export const TYPED_STRINGS = ["Efficient", "Easy", "and", "Affordable"];
+export const BACK_DELAY = 400;
+export const TYPE_SPEED = 150;
+export const LOOP_INFO = true;
+
+export const TEXT_CONTENT = {
+  MAIN_TITLE: "Stay Ahead Of The Curve",
+  SUB_TITLE_PREFIX: "With Our &nbsp;",
+  SUB_TITLE_SUFFIX: "Notes",
+  DESCRIPTION_ONE:
+    "Notes Library is a project dedicated to my friends to help them with their notes.",
+  DESCRIPTION_TWO: " It is your one-stop-shop for getting high quality notes.",
+};
+
+export const EXPLORE_BUTTON_TEXT = "Explore Notes";
