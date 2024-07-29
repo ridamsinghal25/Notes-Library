79e5aab07f3f1efb7163390a6594fb25d7fbc5b9
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-07-29 18:27:23 +0530

    add notes page to codebase

--- Frontend/src/pages/NotesPage.jsx
+++ Frontend/src/pages/NotesPage.jsx
@@ -11,15 +11,30 @@ function NotesPage() {
   }
 
   return (
-    <div>
-      <div>
-        <div className="fixed top-4 right-4">
-          <Button onClick={handleClick}>
-            <Upload />
-          </Button>
-        </div>
-        <UploadNotes showDialog={showDialog} setShowDialog={setShowDialog} />
+    <div className="min-h-screen w-full flex flex-col">
+      <div className="fixed top-4 right-4 z-10">
+        <Button onClick={handleClick}>
+          <Upload />
+        </Button>
       </div>
+      <UploadNotes showDialog={showDialog} setShowDialog={setShowDialog} />
+
+      <header className="p-4 lg:p-6">
+        <h1 className="text-lg font-semibold md:text-2xl">Notes</h1>
+      </header>
+
+      <main className="flex-grow flex p-4 lg:p-6">
+        <div className="w-full flex items-center justify-center rounded-lg border border-dashed shadow-sm">
+          <div className="flex flex-col items-center gap-4 text-center p-8">
+            <h3 className="text-2xl font-bold tracking-tight">
+              Select the notes that suites you
+            </h3>
+            <p className="text-sm text-muted-foreground">
+              You can start learning as soon as you download the notes.
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
