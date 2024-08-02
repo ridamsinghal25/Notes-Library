954b476d339a12b7e411454ecdb3ef3ac80ffe10
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-02 22:38:01 +0530

    bug fix in auth middleware

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -16,7 +16,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken._id).select(
-      "-password -refreshToken"
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
     );
 
     if (!user) {
