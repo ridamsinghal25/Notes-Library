613ea12a4e40215062cce94712c9463bc51cd968
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-02 08:14:49 +0530

    add user validator to validate data receive from user to codebase

---
+++ Backend/src/validators/user.validators.js
@@ -0,0 +1,130 @@
+import { body } from "express-validator";
+import { AvailableUserRoles } from "../constants.js";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+    body("fullName").trim().notEmpty().withMessage("Fullname is required"),
+    body("rollNumber")
+      .trim()
+      .notEmpty()
+      .withMessage("Roll Number is required")
+      .isLength({ min: 11, max: 11 })
+      .withMessage("Roll Number must be exactly 11 characters long"),
+  ];
+};
+
+const userLoginValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+  ];
+};
+
+const userVerifyEmailValidator = () => {
+  return [
+    body("verifyCode")
+      .trim()
+      .notEmpty()
+      .withMessage("verification code is required")
+      .isLength({ min: 6, max: 6 })
+      .withMessage("Verification code must be exactly 6 characters long")
+      .isNumeric()
+      .withMessage("verify code must be a 6 digit number"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
+
+const userResetForgottenPasswordValidator = () => {
+  return [
+    body("resetCode")
+      .trim()
+      .notEmpty()
+      .withMessage("verification code is required")
+      .isLength({ min: 6, max: 6 })
+      .withMessage("Verification code must be exactly 6 characters long")
+      .isNumeric()
+      .withMessage("verify code must be a 6 digit number"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+  ];
+};
+
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+    body("newPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 6 characters"),
+  ];
+};
+
+const userAssignRoleValidator = () => {
+  return [
+    body("role")
+      .trim()
+      .notEmpty()
+      .withMessage("role is required")
+      .isIn(AvailableUserRoles)
+      .withMessage("Invalid user role"),
+    body("rollNumber")
+      .trim()
+      .notEmpty()
+      .withMessage("Roll Number is required")
+      .isLength({ min: 11, max: 11 })
+      .withMessage("Roll Number must be exactly 11 characters long"),
+  ];
+};
+
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userVerifyEmailValidator,
+  userForgotPasswordValidator,
+  userResetForgottenPasswordValidator,
+  userChangeCurrentPasswordValidator,
+  userAssignRoleValidator,
+};
