4cefb652e4f7fd2c8ad99b8778488c0b5f874b8a
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-08 22:47:35 +0530

    add a update course validator to codebase

--- Backend/src/validators/course.validators.js
+++ Backend/src/validators/course.validators.js
@@ -1,12 +1,45 @@
 import { body } from "express-validator";
 
-const courseValidator = () => {
+const createCourseValidator = () => {
   return [
     body("courseName").trim().notEmpty().withMessage("course name is required"),
     body("semester")
       .trim()
       .notEmpty()
       .withMessage("course semester is required"),
+    body("startDate")
+      .trim()
+      .notEmpty()
+      .withMessage("Start date is required")
+      .isISO8601()
+      .withMessage("Invalid start date. Date must be in ISO8601 format"),
+    body("endDate")
+      .trim()
+      .notEmpty()
+      .withMessage("Expiry date is required")
+      .isISO8601()
+      .withMessage("Invalid end date. Date must be in ISO8601 format"),
+  ];
+};
+
+const updateCourseValidator = () => {
+  return [
+    body("oldCourseName")
+      .trim()
+      .notEmpty()
+      .withMessage("old course name is required"),
+    body("oldSemester")
+      .trim()
+      .notEmpty()
+      .withMessage("old course semester is required"),
+    body("newCourseName")
+      .trim()
+      .notEmpty()
+      .withMessage("new course name is required"),
+    body("newSemester")
+      .trim()
+      .notEmpty()
+      .withMessage("new course semester is required"),
     body("startDate")
       .trim()
       .notEmpty()
@@ -38,4 +71,9 @@ const getCourseByNameValidator = () => {
   ];
 };
 
-export { courseValidator, deleteCourseValidator, getCourseByNameValidator };
+export {
+  createCourseValidator,
+  updateCourseValidator,
+  deleteCourseValidator,
+  getCourseByNameValidator,
+};
