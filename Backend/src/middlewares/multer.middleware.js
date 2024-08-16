cbf43f1fefc6f0c087407fd94aa36c5a0a586b50
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-16 16:53:29 +0530

    refactor: multer middleware increase size and add middleware to check error

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import { ApiError } from "../utils/ApiError.js";
 
 const storage = multer.diskStorage({
   destination: function (_, file, cb) {
@@ -17,8 +18,18 @@ function fileFilter(_, file, cb) {
   }
 }
 
+export const handleMulterError = (err, req, res, next) => {
+  if (err) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      throw new ApiError(400, "File size too large. Max size is 10MB.");
+    }
+    throw new ApiError(400, err.message);
+  }
+  next();
+};
+
 export const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
+  limits: { fileSize: 1024 * 1024 * 10 },
   fileFilter,
 });
