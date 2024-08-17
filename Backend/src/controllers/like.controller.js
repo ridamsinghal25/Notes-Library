9f3e40b2ed0aa71233039ef79630bc987d4d3b8b
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-17 18:05:47 +0530

    add like controller to codebase

---
+++ Backend/src/controllers/like.controller.js
@@ -0,0 +1,43 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Notes } from "../models/notes.model.js";
+import { Like } from "../models/like.model.js";
+import mongoose from "mongoose";
+
+const toggleNotesLike = asyncHandler(async (req, res) => {
+  const { notesId } = req.params;
+
+  const notes = await Notes.findById(notesId);
+
+  if (!notes) {
+    throw new ApiError(404, "notes does not exists");
+  }
+
+  const isAlreadyLiked = await Like.findOne({
+    notesId,
+    likedBy: req.user?._id,
+  });
+
+  if (!isAlreadyLiked) {
+    await Like.create({
+      notesId,
+      likedBy: req.user?._id,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { isLiked: true }, "Liked successfully"));
+  } else {
+    await Like.findOneAndDelete({
+      notesId,
+      likedBy: req.user?._id,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { isLiked: false }, "Unliked successfully"));
+  }
+});
+
+export { toggleNotesLike };
