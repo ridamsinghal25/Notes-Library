f2369b8d0eef2f174d78a0ec9c54be5158f4a73d
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-17 22:04:05 +0530

    add express-rate-limiter to codebase

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,32 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
+import requestIp from "request-ip";
 
 const app = express();
+
+app.use(requestIp.mw());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 200,
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req, res) => {
+    return req.clientIp;
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.limit
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
+app.use(limiter);
 
 app.use(
   cors({
@@ -22,6 +46,7 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
 import courseRouter from "./routes/course.route.js";
 import notesRouter from "./routes/notes.route.js";
+import { ApiError } from "./utils/ApiError.js";
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
