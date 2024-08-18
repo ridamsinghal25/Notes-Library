928fd1e097e3e6dba421d55cea03d43f13b879b9
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-18 13:25:51 +0530

    add helper function to remove unused multer files

---
+++ Backend/src/utils/helpers.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+
+export const removeLocalFile = (localPath) => {
+  fs.unlink(localPath, (err) => {
+    if (err) console.log("Error while removing local files: ", err);
+    else {
+      console.log("Removed local file: ", localPath);
+    }
+  });
+};
+
+export const removeUnusedMulterImageFilesOnError = (req) => {
+  try {
+    const multerFile = req.file;
+    const multerFiles = req.files;
+
+    if (multerFile) {
+      removeLocalFile(multerFile.path);
+    }
+
+    if (multerFiles) {
+      const filesValueArray = Object.values(multerFiles);
+
+      filesValueArray.map((fileFields) => {
+        fileFields.map((fileObject) => {
+          removeLocalFile(fileObject.path);
+        });
+      });
+    }
+  } catch (error) {
+    // fail silently
+    console.log("Error while removing image files: ", error);
+  }
+};
