83c4c6b02accb73a499b4ea8325d0216c49a7757
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-19 16:29:30 +0530

    add winston and morgan logger to codebase

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { rateLimit } from "express-rate-limit";
 import requestIp from "request-ip";
+import { ApiError } from "./utils/ApiError.js";
+import morganMiddleware from "./logger/morgan.logger.js";
 
 const app = express();
 
@@ -39,6 +41,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use(morganMiddleware);
 
 import { errorHandler } from "./middlewares/error.middleware.js";
 // import routes
