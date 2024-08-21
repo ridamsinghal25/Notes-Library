8eb26a8092ee29b63d8152daa42b6a497aa0d0be
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-21 21:59:18 +0530

    add update semester component to codebase

---
+++ Frontend/src/components/UpdateSemester.jsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useForm } from "react-hook-form";
+import { Form } from "../components/ui/form";
+import FormFieldSelect from "./FormFieldSelect";
+import { SEMESTER_OPTIONS } from "@/constants/auth";
+import { BUTTONS, UPDATE_SEMESTER_TEXT_CONTENT } from "@/constants/account";
+
+export function UpdateSemester({ showDialog, setShowDialog }) {
+  const updateSemesterForm = useForm({
+    defaultValues: {
+      semester: "",
+    },
+  });
+
+  return (
+    <Dialog open={showDialog} onOpenChange={setShowDialog}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{UPDATE_SEMESTER_TEXT_CONTENT.TITLE}</DialogTitle>
+          <DialogDescription>
+            {UPDATE_SEMESTER_TEXT_CONTENT.DESCRIPTION}
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...updateSemesterForm}>
+          <form
+            onSubmit={updateSemesterForm.handleSubmit()}
+            className="space-y-8"
+          >
+            <FormFieldSelect
+              form={updateSemesterForm}
+              label="Semester"
+              name="semester"
+              values={SEMESTER_OPTIONS}
+              placeholder="Select your semester"
+            />
+            <Button type="submit">{BUTTONS.UPDATE_SEMESTER}</Button>
+          </form>
+        </Form>
+        <DialogFooter></DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
