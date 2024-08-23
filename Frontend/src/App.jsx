555cd8ba267cb742ac7edcb1657b31f12b70fe9f
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-23 22:52:35 +0530

    setup routes in my codebase

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,28 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import LayoutWithSidebar from "./components/Sidebar/LayoutWithSidebar";
+import Home from "./pages/Home";
+import { AccountPage } from "./pages/AccountPage";
+import { ROUTES } from "./constants/route";
+import NotesPage from "./pages/NotesPage";
+import SignupPage from "./pages/SignupPage";
+import SigninPage from "./pages/SigninPage";
+import InputOTPForm from "./pages/InputOTPForm";
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <main className="mx-5 mt-16 lgl:ml-[300px] lgl:mt-3">
-        <div>
-          <Outlet />
-        </div>
-      </main>
-    </div>
+    <Routes>
+      <Route element={<LayoutWithSidebar />}>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.SETTING} element={<AccountPage />} />
+        <Route path={ROUTES.NOTES} element={<NotesPage />} />
+        <Route path={ROUTES.PROFILE} element={<h1>Profile Page</h1>} />
+      </Route>
+      <Route>
+        <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+        <Route path={ROUTES.SIGNIN} element={<SigninPage />} />
+        <Route path={ROUTES.VERIFYCODE} element={<InputOTPForm />} />
+      </Route>
+    </Routes>
   );
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,54 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { AccountPage } from "./pages/AccountPage.jsx";
-import { ROUTES } from "./constants/route.js";
-import Home from "./pages/Home.jsx";
-import NotesPage from "./pages/NotesPage.jsx";
-import SignupPage from "./pages/SignupPage.jsx";
-import SigninPage from "./pages/SigninPage.jsx";
-import InputOTPForm from "./pages/InputOTPForm.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: ROUTES.HOME,
-    element: <App />,
-    children: [
-      {
-        path: ROUTES.HOME,
-        element: <Home />,
-      },
-      {
-        path: ROUTES.SIGNUP,
-        element: <SignupPage />,
-      },
-      {
-        path: ROUTES.SIGNIN,
-        element: <SigninPage />,
-      },
-      {
-        path: ROUTES.PROFILE,
-        element: <h1>Profile Page</h1>,
-      },
-      {
-        path: ROUTES.NOTES,
-        element: <NotesPage />,
-      },
-      {
-        path: ROUTES.SETTING,
-        element: <AccountPage />,
-      },
-      {
-        path: ROUTES.VERIFYCODE,
-        element: <InputOTPForm />,
-      },
-    ],
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
