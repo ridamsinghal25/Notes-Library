dd1e949e2903ade0b095e19c74c1cdc75cdd6d8f
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-29 21:03:06 +0530

    add zod validation that string does not include whitespaces

--- Frontend/src/validation/zodValidation.js
+++ Frontend/src/validation/zodValidation.js
@@ -3,10 +3,11 @@ import { z } from "zod";
 export const signupFormValidation = z.object({
   fullName: z
     .string()
+    .trim()
     .regex(/^[a-zA-Z\s]+$/, "Full name must contain only alphabets and spaces")
     .min(6, "Full name must be atleast 6 characters")
     .max(20, "Full name must not be more than 20 characters"),
-  email: z.string().email("Invalid email address"),
+  email: z.string().trim().email("Invalid email address"),
   rollNumber: z
     .string()
     .trim()
@@ -14,23 +15,27 @@ export const signupFormValidation = z.object({
     .length(11, "Roll number must be exactly 11 digits"),
   password: z
     .string()
+    .trim()
     .min(8, "Password must be atleast 8 characters")
     .max(40, "Password must not be more than 40 characters"),
   courseName: z
     .string()
+    .trim()
     .regex(/^[a-zA-Z\s.-]+$/, "course must contain only alphabets and spaces")
     .min(1, "Course is required"),
   semester: z
     .string()
+    .trim()
     .regex(/^[a-zA-Z\s]+$/, "semester must contain only alphabets and spaces")
     .min(1, "Semester is required")
     .max(20, "Semester must not be more than 20 characters"),
 });
 
 export const signinFormValidation = z.object({
-  email: z.string().email("Invalid email address"),
+  email: z.string().trim().email("Invalid email address"),
   password: z
     .string()
+    .trim()
     .min(8, "Password must be atleast 8 characters")
     .max(40, "Password must not be more than 40 characters"),
 });
@@ -38,6 +43,7 @@ export const signinFormValidation = z.object({
 export const inputOTPValidation = z.object({
   verifyCode: z
     .string()
+    .trim()
     .regex(/^\d{6}$/, "Verification code must contain only digits")
     .length(6, "verification code must be 6 digit"),
 });
@@ -45,15 +51,18 @@ export const inputOTPValidation = z.object({
 export const uploadNotesValidation = z.object({
   subject: z
     .string()
+    .trim()
     .regex(/^[a-zA-Z\s]+$/, "subjects must contain only alphabets and spaces")
     .min(1, "Subject is required")
     .max(20, "Subject must not be more than 20 characters"),
   chapterNumber: z
     .string()
+    .trim()
     .regex(/^\d+$/, "Chapter number must contain only digits")
     .max(2, "Chapter number must not be more than 2 digits"),
   chapterName: z
     .string()
+    .trim()
     .regex(
       /^[a-zA-Z\s]+$/,
       "chapter name must contain only alphabets and spaces"
@@ -62,16 +71,19 @@ export const uploadNotesValidation = z.object({
     .max(20, "Chapter name must not be more than 20 characters"),
   notes: z
     .string()
+    .trim()
     .max(100, "Notes cannot must not be more than 100 characters"),
 });
 
 export const newPasswordFormValidation = z.object({
   currentPassword: z
     .string()
+    .trim()
     .min(8, "Password must be atleast 8 characters")
     .max(40, "Password must not be more than 40 characters"),
   newPassword: z
     .string()
+    .trim()
     .min(8, "Password must be atleast 8 characters")
     .max(40, "Password must not be more than 40 characters"),
 });
@@ -79,6 +91,7 @@ export const newPasswordFormValidation = z.object({
 export const updateSemesterFormValidation = z.object({
   semester: z
     .string()
+    .trim()
     .regex(/^[a-zA-Z\s]+$/, "semester must contain only alphabets and spaces")
     .min(1, "Semester is required")
     .max(20, "Semester must not be more than 20 characters"),
