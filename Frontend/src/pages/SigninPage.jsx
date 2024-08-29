9fb6cbd10fdfec1ea45c3f7e71f706913dcc0e2e
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-29 08:20:35 +0530

    add funtionality to signin page

--- Frontend/src/pages/SigninPage.jsx
+++ Frontend/src/pages/SigninPage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FormFieldInput from "@/components/FormFieldInput";
 import {
   TITLE,
@@ -14,8 +14,18 @@ import {
 import { ROUTES } from "@/constants/route";
 import { signinFormValidation } from "@/validation/zodValidation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import AuthService from "@/services/AuthService";
+import ApiError from "@/services/ApiError";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { login } from "@/store/AuthSlice";
 
 function SigninPage() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const signinForm = useForm({
     resolver: zodResolver(signinFormValidation),
     defaultValues: {
@@ -24,9 +34,25 @@ function SigninPage() {
     },
   });
 
-  function onSubmit(data) {
-    console.log(data);
-  }
+  const onSubmit = async (data) => {
+    setIsSubmitting(true);
+
+    const response = await AuthService.loginService(data.email, data.password);
+
+    setIsSubmitting(false);
+
+    if (!(response instanceof ApiError)) {
+      toast.success(response?.message || "User login successfully");
+
+      const userDetails = response.data?.user;
+
+      dispatch(login({ userDetails }));
+
+      navigate(`${ROUTES.HOME}`);
+    } else {
+      toast.error(response?.errorResponse?.message || response?.errorMessage);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
@@ -52,9 +78,18 @@ function SigninPage() {
               form={signinForm}
               label="Password"
               name="password"
+              type="password"
               placeholder="Enter your password"
             />
-            <Button type="submit">{SIGNIN_BUTTON_TEXT}</Button>
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please Wait
+                </>
+              ) : (
+                SIGNIN_BUTTON_TEXT
+              )}
+            </Button>
           </form>
         </Form>
         <div className="text-center mt-4">
