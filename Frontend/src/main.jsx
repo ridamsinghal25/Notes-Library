80b44909ef8be27f1bb766964e377b41d6605adb
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-29 16:16:57 +0530

    add axios interceptors to refresh token

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,10 +7,41 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import ToastConfig from "./components/ToastConfig";
+import AuthService from "./services/AuthService.js";
+import ApiError from "./services/ApiError.js";
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URI;
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 60000;
+
+let refreshingTokenInProgress = false;
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error?.config?.url?.includes("refresh-token")) {
+      return Promise.reject(error);
+    }
+
+    if (
+      error?.response?.status === 401 &&
+      !error?.config?.url?.includes("login") &&
+      !refreshingTokenInProgress
+    ) {
+      refreshingTokenInProgress = true;
+
+      const response = await AuthService.refreshAccessToken();
+
+      refreshingTokenInProgress = false;
+
+      if (!(response instanceof ApiError)) {
+        return axios(error.config);
+      }
+
+      return Promise.reject(error);
+    }
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
