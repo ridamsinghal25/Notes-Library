e6d848258a03dad5b4e959422f69aa12caf5d06c
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-30 21:44:07 +0530

    refactor zod validation messages

--- Frontend/src/validation/zodValidation.js
+++ Frontend/src/validation/zodValidation.js
@@ -4,7 +4,7 @@ export const signupFormValidation = z.object({
   fullName: z
     .string()
     .trim()
-    .regex(/^[a-zA-Z\s]+$/, "Full name must contain only alphabets and spaces")
+    .regex(/^[a-zA-Z\s]+$/, "Full name must contain only alphabets")
     .min(6, "Full name must be atleast 6 characters")
     .max(20, "Full name must not be more than 20 characters"),
   email: z.string().trim().email("Invalid email address"),
@@ -21,12 +21,12 @@ export const signupFormValidation = z.object({
   courseName: z
     .string()
     .trim()
-    .regex(/^[a-zA-Z\s.-]+$/, "course must contain only alphabets and spaces")
+    .regex(/^[a-zA-Z\s.-]+$/, "course must contain only alphabets")
     .min(1, "Course is required"),
   semester: z
     .string()
     .trim()
-    .regex(/^[a-zA-Z\s]+$/, "semester must contain only alphabets and spaces")
+    .regex(/^[a-zA-Z\s]+$/, "semester must contain only alphabets")
     .min(1, "Semester is required")
     .max(20, "Semester must not be more than 20 characters"),
 });
@@ -52,7 +52,7 @@ export const uploadNotesValidation = z.object({
   subject: z
     .string()
     .trim()
-    .regex(/^[a-zA-Z\s]+$/, "subjects must contain only alphabets and spaces")
+    .regex(/^[a-zA-Z\s]+$/, "subjects must contain only alphabets")
     .min(1, "Subject is required")
     .max(20, "Subject must not be more than 20 characters"),
   chapterNumber: z
@@ -63,10 +63,7 @@ export const uploadNotesValidation = z.object({
   chapterName: z
     .string()
     .trim()
-    .regex(
-      /^[a-zA-Z\s]+$/,
-      "chapter name must contain only alphabets and spaces"
-    )
+    .regex(/^[a-zA-Z\s]+$/, "chapter name must contain only alphabets")
     .min(1, "Chapter name is required")
     .max(20, "Chapter name must not be more than 20 characters"),
   notes: z
@@ -92,7 +89,7 @@ export const updateSemesterFormValidation = z.object({
   semester: z
     .string()
     .trim()
-    .regex(/^[a-zA-Z\s]+$/, "semester must contain only alphabets and spaces")
+    .regex(/^[a-zA-Z\s]+$/, "semester must contain only alphabets")
     .min(1, "Semester is required")
     .max(20, "Semester must not be more than 20 characters"),
 });
