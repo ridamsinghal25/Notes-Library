6169f310478ceb5a0a419dcf2062315f53a43159
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-31 21:31:28 +0530

    add like rate limiter to like routes and refactor user routes

--- Backend/src/routes/like.route.js
+++ Backend/src/routes/like.route.js
@@ -3,13 +3,38 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { validate } from "../validators/validate.js";
 import { mongoIdPathVariableValidator } from "../validators/mongodb.validators.js";
 import { toggleNotesLike } from "../controllers/like.controller.js";
+import { rateLimit } from "express-rate-limit";
+import { ApiError } from "../utils/ApiError.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
+const likeRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req, _) => {
+    return req.clientIp;
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.limit
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
 router
   .route("/notes/:notesId")
-  .post(mongoIdPathVariableValidator("notesId"), validate, toggleNotesLike);
+  .post(
+    likeRateLimiter,
+    mongoIdPathVariableValidator("notesId"),
+    validate,
+    toggleNotesLike
+  );
 
 export default router;
