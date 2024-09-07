e96d8902ee85ff8c1d1ac5fd8ee2a00864988d58
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-09-07 07:54:14 +0530

    refactor  page layout component

--- Frontend/src/components/PageLayout.jsx
+++ Frontend/src/components/PageLayout.jsx
@@ -1,16 +1,12 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useCallback, useEffect } from "react";
 import AuthService from "@/services/AuthService";
 import ApiError from "@/services/ApiError";
 import { login, logout, updateLoginCheckDone } from "@/store/AuthSlice";
-import { ROUTES } from "@/constants/route";
-import { toast } from "react-toastify";
 
 function PageLayout() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
 
   const fetchUser = useCallback(async () => {
     const response = await AuthService.getCurrentUser(
@@ -22,17 +18,6 @@ function PageLayout() {
 
     if (!(response instanceof ApiError)) {
       dispatch(login(response?.data));
-    } else if (
-      response instanceof ApiError &&
-      response?.errorResponse?.message === "Please verify your email" &&
-      !(
-        location.pathname === ROUTES.SIGNUP ||
-        location.pathname === ROUTES.VERIFYCODE
-      )
-    ) {
-      dispatch(logout());
-      navigate(`${ROUTES.VERIFYCODE}`);
-      toast.error(response?.errorResponse?.message);
     } else {
       dispatch(logout());
     }
