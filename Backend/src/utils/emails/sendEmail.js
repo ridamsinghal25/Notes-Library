d4a26793998cf10941e7e6989bb10cdfa7066aef
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-09-09 17:46:49 +0530

    add nodemailer to send email

--- Backend/src/utils/emails/sendEmail.js
+++ Backend/src/utils/emails/sendEmail.js
@@ -1,14 +1,30 @@
-import { resend } from "../resend.js";
+import nodemailer from "nodemailer";
 
 export async function sendEmail(options) {
   try {
-    await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+
+    let mailOptions = {
+      from: {
+        name: "Notes Library",
+        address: process.env.USER,
+      },
       to: options.email,
       subject: options.subject,
       html: options.htmlContent,
       text: options.textContent,
-    });
+    };
+
+    await transporter.sendMail(mailOptions);
 
     return { success: true, message: "Verification email sent successfully." };
   } catch (Emailerror) {
