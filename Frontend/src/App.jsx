82accde73d13577e25bf3abeac809f401574f3de
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-09-11 19:26:16 +0530

    apply lazy loading to notes subject page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import PageLayout from "./components/PageLayout";
 import Home from "./pages/Home";
-import { AccountPage } from "./pages/AccountPage";
+import AccountPage from "./pages/AccountPage";
 import { ROUTES } from "./constants/route";
 import NotesPage from "./pages/NotesPage";
 import SignupPage from "./pages/SignupPage";
@@ -10,11 +10,13 @@ import InputOTPForm from "./pages/InputOTPForm";
 import ForLoggedInUsers from "./protectedRoutes/ForLoggedInUsers";
 import PublicRoutes from "./protectedRoutes/PublicRoutes";
 import LayoutWithSidebar from "./components/Sidebar/LayoutWithSidebar";
-import NotesSubjectPage from "./pages/NotesSubjectPage";
 import ProfilePage from "./pages/ProfilePage";
 import ForgotPassword from "./pages/ForgotPasswordPage";
 import PageNotFound from "./pages/PageNotFound";
 import OverviewPage from "./pages/OverviewPage";
+import { lazy, Suspense } from "react";
+
+const NotesSubjectPage = lazy(() => import("./pages/NotesSubjectPage"));
 
 function App() {
   return (
@@ -25,7 +27,14 @@ function App() {
             <Route path={ROUTES.HOME} element={<Home />} />
             <Route path={ROUTES.SETTING} element={<AccountPage />} />
             <Route path={ROUTES.NOTES} element={<NotesPage />} />
-            <Route path={ROUTES.NOTES_SUBJECT} element={<NotesSubjectPage />} />
+            <Route
+              path={ROUTES.NOTES_SUBJECT}
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <NotesSubjectPage />
+                </Suspense>
+              }
+            />
             <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
           </Route>
         </Route>
