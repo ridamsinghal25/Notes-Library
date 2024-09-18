819b461ad4ef88c866a55abdd544e0ec309b12c2
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-09-18 19:42:46 +0530

    refactor forgot password page: add dark mode

--- Frontend/src/pages/ForgotPasswordPage.jsx
+++ Frontend/src/pages/ForgotPasswordPage.jsx
@@ -16,6 +16,7 @@ import { Loader2 } from "lucide-react";
 import AuthService from "@/services/AuthService";
 import ApiError from "@/services/ApiError";
 import { toast } from "react-toastify";
+import Container from "@/components/Container";
 
 function ForgotPassword() {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -50,48 +51,50 @@ function ForgotPassword() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-800">
-      <div className="w-full max-w-md p-6 sm:p-8 lg:p-10 space-y-6 sm:space-y-8 bg-white rounded-lg shadow-md my-6 sm:my-10">
-        <div className="text-center">
-          <h1 className="text-2xl sm:text-2xl lg:text-4xl font-extrabold tracking-tight mb-4 sm:mb-6">
-            {FORGOT_PASSWORD_TITLE}
-          </h1>
-          <p className="mb-4">{FORGOT_PASSWORD_DESCRIPTION}</p>
+    <Container>
+      <div className="flex justify-center items-center min-h-screen mx-4">
+        <div className="w-full max-w-md p-6 sm:p-8 lg:p-10 space-y-6 sm:space-y-8 rounded-lg shadow-md my-6 sm:my-10 dark:border-gray-500 dark:border-2">
+          <div className="text-center dark:text-gray-200">
+            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-extrabold tracking-tight mb-4 sm:mb-6">
+              {FORGOT_PASSWORD_TITLE}
+            </h1>
+            <p className="mb-4">{FORGOT_PASSWORD_DESCRIPTION}</p>
+          </div>
+          <Form {...resetPasswordForm}>
+            <form
+              onSubmit={resetPasswordForm.handleSubmit(onResetPassword)}
+              className="space-y-6"
+            >
+              <FormFieldInput
+                form={resetPasswordForm}
+                label="Reset Code"
+                name="resetCode"
+                placeholder="Enter your reset code"
+              />
+              <FormFieldInput
+                form={resetPasswordForm}
+                label="Password"
+                type="password"
+                name="newPassword"
+                placeholder="Enter your password"
+              />
+              <div className="w-full flex justify-end">
+                <Button type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
+                      Wait
+                    </>
+                  ) : (
+                    FORGOT_PASSWORD_BUTTON_TEXT
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
         </div>
-        <Form {...resetPasswordForm}>
-          <form
-            onSubmit={resetPasswordForm.handleSubmit(onResetPassword)}
-            className="space-y-6"
-          >
-            <FormFieldInput
-              form={resetPasswordForm}
-              label="Reset Code"
-              name="resetCode"
-              placeholder="Enter your reset code"
-            />
-            <FormFieldInput
-              form={resetPasswordForm}
-              label="Password"
-              type="password"
-              name="newPassword"
-              placeholder="Enter your password"
-            />
-            <div className="w-full flex justify-end">
-              <Button type="submit" disabled={isSubmitting}>
-                {isSubmitting ? (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
-                    Wait
-                  </>
-                ) : (
-                  FORGOT_PASSWORD_BUTTON_TEXT
-                )}
-              </Button>
-            </div>
-          </form>
-        </Form>
       </div>
-    </div>
+    </Container>
   );
 }
 
