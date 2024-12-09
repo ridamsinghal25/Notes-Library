cb63dfe6cf7e7045679916c5c5ea4817a88103a4
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-12-09 13:00:21 +0530

    refactor comment controller

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -3,10 +3,12 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { getComment } from "../models/comment.model.js";
 import { getNotes } from "../models/notes.model.js";
+import { getUser } from "../models/user.model.js";
 
 const createComment = asyncHandler(async (req, res) => {
   const Comment = await getComment();
   const Notes = await getNotes();
+  const User = await getUser();
 
   const { notesId } = req.params;
   const { content } = req.body;
@@ -27,6 +29,14 @@ const createComment = asyncHandler(async (req, res) => {
     throw new ApiError(500, "failed to create comment");
   }
 
+  const user = await User.findById(req.user?._id);
+
+  if (!user) {
+    throw new ApiError(404, "user does not exists");
+  }
+
+  newComment.owner = user;
+
   return res
     .status(200)
     .json(new ApiResponse(200, newComment, "comment created successfully"));
@@ -34,6 +44,7 @@ const createComment = asyncHandler(async (req, res) => {
 
 const editComment = asyncHandler(async (req, res) => {
   const Comment = await getComment();
+  const User = await getUser();
 
   const { commentId } = req.params;
   const { content } = req.body;
@@ -51,6 +62,14 @@ const editComment = asyncHandler(async (req, res) => {
   if (!updatedComment) {
     throw new ApiError(500, "This comment does not exists");
   }
+
+  const user = await User.findById(req.user?._id);
+
+  if (!user) {
+    throw new ApiError(404, "user does not exists");
+  }
+
+  updatedComment.owner = user;
 
   return res
     .status(200)
@@ -76,6 +95,7 @@ const deleteComment = asyncHandler(async (req, res) => {
 const getComments = asyncHandler(async (req, res) => {
   const Comment = await getComment();
   const Notes = await getNotes();
+  const User = await getUser();
 
   const { notesId } = req.params;
 
@@ -87,13 +107,28 @@ const getComments = asyncHandler(async (req, res) => {
 
   const comments = await Comment.find({ notesId });
 
+  const ownerIds = comments.map((comment) => comment.owner);
+  const users = await User.find({ _id: { $in: ownerIds } });
+
+  const userMap = users.reduce((map, user) => {
+    map[user._id] = user;
+    return map;
+  }, {});
+
+  const commentsWithOwners = comments.map((comment) => ({
+    ...comment.toObject(),
+    owner: userMap[comment.owner] || null,
+  }));
+
   if (!comments || comments.length === 0) {
     return res.status(200).json(new ApiResponse(200, [], "No comments posted"));
   }
 
   return res
     .status(200)
-    .json(new ApiResponse(200, comments, "comments fetched successfully"));
+    .json(
+      new ApiResponse(200, commentsWithOwners, "comments fetched successfully")
+    );
 });
 
 export { createComment, editComment, deleteComment, getComments };
