6fc659997f831324eec212987ee8c1018d7a6a49
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-12-25 17:44:54 +0530

    refactor auth middleware verify jwt method

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -18,7 +18,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken._id).select(
-      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+      "-password -emailVerificationToken -emailVerificationExpiry -refreshToken -forgotPasswordToken -forgotPasswordExpiry"
     );
 
     if (!user) {
