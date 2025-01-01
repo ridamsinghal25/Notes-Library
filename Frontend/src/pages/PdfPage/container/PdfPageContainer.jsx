5a35ae331ab9655ed79d959416764bd57637c75d
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-01-01 15:40:30 +0530

    refactor pdf page

--- Frontend/src/pages/PdfPage/container/PdfPageContainer.jsx
+++ Frontend/src/pages/PdfPage/container/PdfPageContainer.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef } from "react";
-import WebViewer from "@pdftron/pdfjs-express-viewer";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import PDFPage from "../presentation/PdfPage";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "@/constants/route";
-import PDFPage from "../presentation/PdfPage";
 
 function PdfPageContainer() {
-  const viewer = useRef(null);
-  const instance = useRef(null);
   const selectedNotes = useSelector((state) => state.modal.selectedNotes);
+  const theme = useSelector((state) => state.theme?.theme);
+
   const navigate = useNavigate();
   const pdfUrl = selectedNotes?.pdf?.url;
 
@@ -18,55 +17,87 @@ function PdfPageContainer() {
     }
   }, [pdfUrl]);
 
-  useEffect(() => {
-    if (!pdfUrl) return;
+  const docs = [
+    {
+      uri: pdfUrl,
+      fileName: selectedNotes?.chapterName,
+    },
+  ];
 
-    // Initialize WebViewer only once
-    WebViewer(
-      {
-        path: "../../../../../webviewer",
-        initialDoc: pdfUrl,
-        licenseKey: "dGiXN50WjhorEDQUkX93",
-      },
-      viewer.current
-    ).then((inst) => {
-      instance.current = inst;
-
-      // Set WebViewer to dark mode
-      inst.UI.setTheme("dark");
-
-      // Set the hand tool as the default tool
-      inst.setToolMode("Pan");
-
-      // Lazy load pages on demand
-      inst.loadDocument(pdfUrl, { onLoad: "on-demand" });
-
-      // Disable annotation functionality initially
-      inst.annotManager.disableAnnotations();
-
-      // Disable annotation-related UI elements
-      inst.UI.disableElements([
-        "annotationPopup",
-        "contextMenuPopup",
-        "toolsButton",
-        "textPopup",
-        "searchButton",
-        "selectToolButton",
-        "viewControlsButton",
-      ]);
-
-      inst.UI.setLayoutMode(inst.UI.LayoutMode.Single);
-
-      // Remove annotation tools from the header toolbar
-      inst.UI.setHeaderItems((header) => {
-        header.delete("freeHandToolGroupButton");
-        header.delete("stickyToolGroupButton");
-        header.delete("shapeToolGroupButton");
-      });
-    });
-  }, [pdfUrl]);
-
-  return <PDFPage viewer={viewer} />;
+  return <PDFPage docs={docs} theme={theme} />;
 }
 
 export default PdfPageContainer;
+
+// import React, { useEffect, useRef } from "react";
+// import WebViewer from "@pdftron/pdfjs-express-viewer";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { ROUTES } from "@/constants/route";
+// import PDFPage from "../presentation/PdfPage";
+
+// function PdfPageContainer() {
+//   const viewer = useRef(null);
+//   const instance = useRef(null);
+//   const selectedNotes = useSelector((state) => state.modal.selectedNotes);
+//   const navigate = useNavigate();
+//   const pdfUrl = selectedNotes?.pdf?.url;
+
+//   useEffect(() => {
+//     if (!pdfUrl) {
+//       navigate(ROUTES.NOTES);
+//     }
+//   }, [pdfUrl]);
+
+//   useEffect(() => {
+//     if (!pdfUrl) return;
+
+//     // Initialize WebViewer only once
+//     WebViewer(
+//       {
+//         path: "../../../../../webviewer",
+//         initialDoc: pdfUrl,
+//         licenseKey: "dGiXN50WjhorEDQUkX93",
+//       },
+//       viewer.current
+//     ).then((inst) => {
+//       instance.current = inst;
+
+//       // Set WebViewer to dark mode
+//       inst.UI.setTheme("dark");
+
+//       // Set the hand tool as the default tool
+//       inst.setToolMode("Pan");
+
+//       // Lazy load pages on demand
+//       inst.loadDocument(pdfUrl, { onLoad: "on-demand" });
+
+//       // Disable annotation functionality initially
+//       inst.annotManager.disableAnnotations();
+
+//       // Disable annotation-related UI elements
+//       inst.UI.disableElements([
+//         "annotationPopup",
+//         "contextMenuPopup",
+//         "toolsButton",
+//         "textPopup",
+//         "searchButton",
+//         "selectToolButton",
+//         "viewControlsButton",
+//       ]);
+
+//       inst.UI.setLayoutMode(inst.UI.LayoutMode.Single);
+
+//       // Remove annotation tools from the header toolbar
+//       inst.UI.setHeaderItems((header) => {
+//         header.delete("freeHandToolGroupButton");
+//         header.delete("stickyToolGroupButton");
+//         header.delete("shapeToolGroupButton");
+//       });
+//     });
+//   }, [pdfUrl]);
+
+//   return <PDFPage viewer={viewer} />;
+// }
+
+// export default PdfPageContainer;
--- Frontend/src/pages/PdfPage/presentation/PdfPage.jsx
+++ Frontend/src/pages/PdfPage/presentation/PdfPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import "@cyntler/react-doc-viewer/dist/index.css";
 
-function PDFPage({ viewer }) {
+function PDFPage({ docs, theme }) {
   return (
-    <div className="pt-14 pb-6 max-w-screen sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-xl h-screen">
+    <div className="mt-5">
       <HelmetProvider>
         <Helmet>
           <title>Pdf Notes</title>
@@ -11,9 +13,47 @@ function PDFPage({ viewer }) {
           <meta name="description" content="This is a pdf page" />
         </Helmet>
       </HelmetProvider>
-      <div className="h-full w-full" ref={viewer}></div>
+      <DocViewer
+        documents={docs}
+        pluginRenderers={DocViewerRenderers}
+        theme={{
+          primary: "#5296d8",
+          secondary: "#ffffff",
+          tertiary: "#5296d899",
+          textPrimary: "#ffffff",
+          textSecondary: "#5296d8",
+          textTertiary: "#00000099",
+          disableThemeScrollbar: false,
+        }}
+        config={{
+          pdfZoom: {
+            defaultZoom: 0.8,
+          },
+          pdfVerticalScrollByDefault: true,
+        }}
+        style={{
+          backgroundColor: `${theme === "dark" ? "#28282B" : "#FAF9F6"}`,
+        }}
+      />
     </div>
   );
 }
 
 export default PDFPage;
+
+// function PDFPage({ viewer }) {
+//   return (
+//     <div className="pt-14 pb-6 max-w-screen sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-xl h-screen">
+//       <HelmetProvider>
+//         <Helmet>
+//           <title>Pdf Notes</title>
+//           <meta charset="UTF-8" />
+//           <meta name="description" content="This is a pdf page" />
+//         </Helmet>
+//       </HelmetProvider>
+//       <div className="h-full w-full" ref={viewer}></div>
+//     </div>
+//   );
+// }
+
+// export default PDFPage;
