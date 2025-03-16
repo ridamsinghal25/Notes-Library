367f2884b372b37548a9812e96649df8314b829d
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-03-16 11:47:46 +0530

    feat: add AppLoader component and integrate loading state in PageLayout

--- Frontend/src/layouts/PageLayout.jsx
+++ Frontend/src/layouts/PageLayout.jsx
@@ -1,35 +1,43 @@
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AuthService from "@/services/AuthService";
 import ApiError from "@/services/ApiError";
 import { login, logout, updateLoginCheckDone } from "@/store/AuthSlice";
+import AppLoader from "@/components/basic/AppLoader";
 
 function PageLayout() {
   const dispatch = useDispatch();
+  const [isFetching, setIsFetching] = useState(false);
 
   const fetchUser = useCallback(async () => {
-    const response = await AuthService.getCurrentUser(
-      {},
-      {
-        "Cache-Control": "no-cache, no-store, must-revalidate",
+    try {
+      setIsFetching(true);
+      const response = await AuthService.getCurrentUser(
+        {},
+        {
+          "Cache-Control": "no-cache, no-store, must-revalidate",
+        }
+      );
+
+      if (!(response instanceof ApiError)) {
+        dispatch(login(response?.data));
+      } else {
+        dispatch(logout());
       }
-    );
-
-    if (!(response instanceof ApiError)) {
-      dispatch(login(response?.data));
-    } else {
+    } catch (error) {
       dispatch(logout());
+    } finally {
+      setIsFetching(false);
+      dispatch(updateLoginCheckDone(true));
     }
-
-    dispatch(updateLoginCheckDone(true));
   }, [dispatch]);
 
   useEffect(() => {
     fetchUser();
   }, [fetchUser]);
 
-  return <Outlet />;
+  return isFetching ? <AppLoader /> : <Outlet />;
 }
 
 export default PageLayout;
