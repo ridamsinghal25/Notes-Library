79d78b81cee565fabae1615f67c87cbb38962acd
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-05-27 18:18:03 +0530

    feat: replace nodemailer with resend for email sending functionality

--- Backend/src/utils/emails/sendEmail.js
+++ Backend/src/utils/emails/sendEmail.js
@@ -1,35 +1,18 @@
-import nodemailer from "nodemailer";
+import { resend } from "../resend.js";
 
 export async function sendEmail(options) {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.USER,
-        pass: process.env.APP_PASSWORD,
-      },
-    });
-
-    let mailOptions = {
-      from: {
-        name: "Notes Library",
-        address: process.env.USER,
-      },
-      to: options.email,
+    await resend.emails.send({
+      from: "Notes Library <support@noteslibrary.tech>",
+      to: [options.email],
       subject: options.subject,
       html: options.htmlContent,
       text: options.textContent,
-    };
-
-    await transporter.sendMail(mailOptions);
+    });
 
     return { success: true, message: "Verification email sent successfully." };
-  } catch (Emailerror) {
-    console.log("Error occured while sending verification email: ", Emailerror);
-
+  } catch (error) {
+    console.log("Error occured while sending email: ", error);
     return { success: false, message: "Failed to send verification email." };
   }
 }
